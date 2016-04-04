6ad9d70523874a71ee706b92918d61fd0a6b56b9
Christopher C Strelioff <chris.strelioff@gmail.com>
2016-04-04 16:00:59 -0700

    define setPrivate method

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -40,4 +40,17 @@ Meteor.methods({
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      // Make sure only the task owner can make a task private
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  },
 });
