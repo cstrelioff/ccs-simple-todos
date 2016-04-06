fa7e5aaf70af86d6e6ba0c70085a9a2e0089c79c
Christopher C Strelioff <chris.strelioff@gmail.com>
2016-04-05 17:05:57 -0700

    add test of tasks.remove method; mv tasks.tests.js to correct location

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
 
 import { Tasks } from './tasks.js';
 
@@ -20,7 +21,17 @@ if (Meteor.isServer) {
       });
 
       it('can delete owned task', () => {
+        // Find the internal implementation of the tasks.remove method
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
 
+        // Set up a fake method invocation
+        const invocation = { userId };
+
+        // Run the method with `this` set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
       });
     });
   });
